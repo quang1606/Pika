b12ffe6afac7cd7e2ad30dcffa897373066bff65
quang1606 <quangnguyenxuanymnb2004@gmail.com>
2025-04-20 14:17:04 +0700

    up

--- src/com/pikapika/utils/Utils.java
+++ src/com/pikapika/utils/Utils.java
@@ -1,8 +1,6 @@
 package com.pikapika.utils;
 
-/**
- * Created by anonymousjp on 5/20/17.
- */
+
 public class Utils {
     public static final int WINDOW_WIDTH = 720;
     public static final int WINDOW_HEIGHT = 460;
@@ -21,6 +19,19 @@ public class Utils {
     public static final int MAP_COL = 12;
     public static final int PIKACHU_NUMBER = 34;
     public static final boolean DEBUG = true;
+    // Ví dụ kích thước cho độ khó Easy
+    public static final int EASY_ROWS = 6;  // Ví dụ: 6 hàng Pikachu hiển thị + 2 hàng viền
+    public static final int EASY_COLS = 8; // Ví dụ: 8 cột Pikachu hiển thị + 2 cột viền
+
+    // Ví dụ kích thước cho độ khó Medium (Có thể bằng MAP_ROW/MAP_COL cũ nếu muốn)
+    public static final int MEDIUM_ROWS = 8; // Ví dụ: 8 hàng Pikachu hiển thị + 2 hàng viền
+    public static final int MEDIUM_COLS = 10; // Ví dụ: 10 cột Pikachu hiển thị + 2 cột viền
+
+    // Ví dụ kích thước cho độ khó Hard
+    public static final int HARD_ROWS = 10; // Ví dụ: 10 hàng Pikachu hiển thị + 2 hàng viền
+    public static final int HARD_COLS = 12; // Ví dụ: 14 cột Pikachu hiển thị + 2 cột viền
+
+
     // Trong file com/pikapika/utils/Utils.java
     public static final String BT_AUTO_PLAY = "Tự Động Chơi"; // Hoặc "Auto Play"
     public static void debug(Class clz,String debug){
